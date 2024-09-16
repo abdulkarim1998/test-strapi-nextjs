ad84b3a325e64f24284856a5a995cd8c6ce3419e
Abdul Karim Al Khayari <abdulkarim.khayari@rihal.om>
2024-09-16 10:54:12 +0400

    init strapi with blogs

--- strapi/types/generated/components.d.ts
+++ strapi/types/generated/components.d.ts
@@ -1,5 +1,56 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface TextPhotoSideTextPhotoOnSide extends Schema.Component {
+  collectionName: 'components_text_photo_side_text_photo_on_sides';
+  info: {
+    displayName: 'Text Photo on Side';
+    description: '';
+  };
+  attributes: {
+    content: Attribute.Text & Attribute.Required;
+    Image: Attribute.Media<'images'> & Attribute.Required;
+    ParagraphType: Attribute.Enumeration<['TextPhotoSide']> &
+      Attribute.DefaultTo<'TextPhotoSide'>;
+    order: Attribute.Integer &
+      Attribute.Required &
+      Attribute.SetMinMax<
+        {
+          min: 1;
+        },
+        number
+      > &
+      Attribute.DefaultTo<1>;
+  };
+}
+
+export interface TextPhotoBelowTextPhotoBelow extends Schema.Component {
+  collectionName: 'components_text_photo_below_text_photo_belows';
+  info: {
+    displayName: 'Text Photo Below';
+    icon: 'arrowDown';
+    description: '';
+  };
+  attributes: {
+    content: Attribute.Text & Attribute.Required;
+    image: Attribute.Media<'images'> & Attribute.Required;
+    ParagraphType: Attribute.Enumeration<['TextPhotoBelow']> &
+      Attribute.DefaultTo<'TextPhotoBelow'>;
+    order: Attribute.Integer &
+      Attribute.Required &
+      Attribute.SetMinMax<
+        {
+          min: 1;
+        },
+        number
+      >;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'text-photo-side.text-photo-on-side': TextPhotoSideTextPhotoOnSide;
+      'text-photo-below.text-photo-below': TextPhotoBelowTextPhotoBelow;
+    }
+  }
 }
--- strapi/types/generated/contentTypes.d.ts
+++ strapi/types/generated/contentTypes.d.ts
@@ -362,6 +362,37 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiBlogBlog extends Schema.CollectionType {
+  collectionName: 'blogs';
+  info: {
+    singularName: 'blog';
+    pluralName: 'blogs';
+    displayName: 'blog';
+    description: '';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    TextPhotoBelow: Attribute.Component<
+      'text-photo-below.text-photo-below',
+      true
+    >;
+    TextPhotoSide: Attribute.Component<
+      'text-photo-side.text-photo-on-side',
+      true
+    >;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<'api::blog.blog', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<'api::blog.blog', 'oneToOne', 'admin::user'> &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -798,6 +829,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::blog.blog': ApiBlogBlog;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
